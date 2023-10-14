d986ba6ead7c28e5fec2f758dbf4f69c13937812
Barry-san <oyeyemimubarak6@gmail.com>
2023-10-14 18:13:02 +0100

    removed devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import AppRoutes from "./routes";
 import { Toaster } from "react-hot-toast";
 
@@ -12,7 +11,6 @@ function App() {
         <AppRoutes />
         <Toaster />
       </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
 }
