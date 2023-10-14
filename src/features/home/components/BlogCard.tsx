423c4a181228f9c76cf3c42f6157dde49f924f56
Barry-san <oyeyemimubarak6@gmail.com>
2023-10-14 18:14:14 +0100

    improved the homepage layout and blogcard appearance

--- src/features/home/components/BlogCard.tsx
+++ src/features/home/components/BlogCard.tsx
@@ -9,14 +9,14 @@ type BlogCardProps = {
 
 const BlogCard = ({ id, data, Key }: BlogCardProps) => {
   return (
-    <div className="flex flex-col gap-2  p-2">
-      <Link to={`/post/${id}`} key={Key}>
+    <div className="flex flex-col gap-4 p-2 border border-collapse" key={Key}>
+      <p className=" border-gray-400 p-4">{data.head.category}</p>
+      <Link to={`/post/${id}`} className="flex flex-col gap-2 items-center">
         <img src={data.head.thumbnail} alt="" className="" />
-        <h1 className="text-lg font-sans font-bold">{data.head.title}</h1>
-        <p className="font-mono border-gray-400 p-4">{data.head.category}</p>
-        <p className="underline uppercase">{data.head.author}</p>
-        <p>{data.head.description}</p>
+        <h1 className="text-lg font-bold">{data.head.title}</h1>
+        <p className="underline italic">{data.head.author}</p>
       </Link>
+      <p>{data.head.description}</p>
     </div>
   );
 };
--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -2,15 +2,13 @@ import { GetBlogList } from "../api/getBlogList";
 import { postData } from "src/features/posts/types";
 import BlogCard from "./BlogCard";
 import { Layout } from "components/layout/Layout";
-// import { Loading } from "components/loading/Loading";
 
 const Home = () => {
   const { data, error, isLoading } = GetBlogList();
   return (
     <Layout>
-      <div className="grid grid-cols-1  md:grid-cols-2 gap-4 mx-auto p-4 min-h-full">
+      <div className="grid grid-cols-1  md:grid-cols-2  lg:grid-cols-3 gap-4 mx-auto  min-h-full ">
         {isLoading && "loading..."}
-        {/* <Loading /> */}
         {data
           ? data.docs.map((doc) => {
               const blog = doc.data() as postData;
