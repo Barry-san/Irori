c8edd8b685f97a4e7a80d9c658c27985e1644398
Barry-san <oyeyemimubarak6@gmail.com>
2023-10-16 22:23:40 +0100

    fixed registration logic

--- src/features/auth/api/addUserToDb.ts
+++ src/features/auth/api/addUserToDb.ts
@@ -5,13 +5,11 @@ import { User } from "firebase/auth";
 
 export const addUserToDb = (user: User) => {
   const userData = {
-    userId: user?.uid,
+    userId: user.uid,
     userName: user.displayName,
     email: user.email,
     joinDate: new Date().toDateString(),
   };
   const userRef = doc(db, `users/${userData.userId}`);
-  setDoc(userRef, userData)
-    .then(() => alert("user successfully added to db"))
-    .catch(() => alert("failed to addUser"));
+  setDoc(userRef, userData).catch(() => alert("something went wrong"));
 };
