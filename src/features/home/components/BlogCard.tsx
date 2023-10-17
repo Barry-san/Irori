e037ac0c38a23f67aa3c188b424b9b2313552a2b
Barry-san <oyeyemimubarak6@gmail.com>
2023-10-17 23:11:01 +0100

    refined blogcard

--- src/features/home/components/BlogCard.tsx
+++ src/features/home/components/BlogCard.tsx
@@ -9,13 +9,16 @@ type BlogCardProps = {
 
 const BlogCard = ({ id, data, Key }: BlogCardProps) => {
   return (
-    <div className="flex flex-col p-2 border border-indigo-400 " key={Key}>
+    <div
+      className="flex flex-col p-2 border gap-4 border-indigo-400 rounded-lg"
+      key={Key}
+    >
       {/* <p className="p-4">{data.head.category}</p> */}
-      <Link to={`/post/${id}`} className="flex flex-col gap-2 items-center">
+      <Link to={`/post/${id}`} className="flex flex-col gap-2">
         <img
           src={data.head.thumbnail}
           alt=""
-          className="aspect-video object-cover object-center rounded-md"
+          className="aspect-video object-cover object-center rounded-lg"
         />
         <h1 className="text-lg font-display font-bold">{data.head.title}</h1>
         <p className="underline italic">
